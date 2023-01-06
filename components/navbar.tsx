52af9622ce56ff45233c0a5f91ee60dca29771f8
jonathanchang <jonathan.chang@livibank.com>
2023-01-06 16:54:20 +0800

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ interface NavTabProps {
 const NavTab = ({ tab, current, title }: NavTabProps) => {
     return (
         // <div className={`w-full p-4 flex flex-row justify-center hover:border-r-4 border-primary ${tab === current ? 'border-r-4 ' : ''}`}>
-        <Title className={`w-full p-4 whitespace-nowrap text-md font-black ${tab === current ? 'text-primary' : 'text-slate-300'} hover:text-primary hover:border-r-4 border-primary ${tab === current ? 'border-r-4 ' : ''}}`}>{title}</Title>
+        <Title className={`w-full p-3 whitespace-nowrap text-sm font-bold ${tab === current ? 'text-primary' : 'text-slate-300'} hover:text-primary hover:border-r-4 border-primary ${tab === current ? 'border-r-4 ' : ''}}`}>{title}</Title>
         // </div>
     )
 }
@@ -28,7 +28,7 @@ const navList = [
 const NavBar = ({ tab }: NavbarProps) => {
     return (
         <Card override className="w-1/4 pt-5">
-            <CardBody className="h-full ">
+            <CardBody className="h-full  ">
                 <Stack className="justify-between h-full">
                     <Stack spacing="lg">
                         {navList.map((nav) => {
